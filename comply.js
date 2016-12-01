5b7652bb23f5233fd0dcc2f3da8ce187208f1b47
Brian Ellis <brian.ellis@blueacorn.com>
2016-12-01 11:38:09 -0500

    Update comply.js

--- comply.js
+++ comply.js
@@ -1,7 +1,20 @@
 /**
- * @author      Brian Ellis brianEllisSells@gmail.com
- * @copyright   Copyright © 2016 Brian Ellis.
- * @Description A jQuery plugin for ADA Compliancy and ARIA labeling
+ This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Project: ComplyJS  
+  Copyright (C) 2016  
+  Author: Brian L. Ellis
 **/
 
 jQuery(document).ready(function($){ 
